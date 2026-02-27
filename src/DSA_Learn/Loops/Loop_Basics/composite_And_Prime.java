df2895a4bbd37d181a2ca4417ff142738f07c01d
Abinash Bhutia <abinashbhutia78@gmail.com>
2026-02-27 23:52:14 +0530

    Original now

--- src/DSA_Learn/Loops/Loop_Basics/composite_And_Prime.java
+++ src/DSA_Learn/Loops/Loop_Basics/composite_And_Prime.java
@@ -8,7 +8,7 @@ public class composite_And_Prime {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number: ");
         int n = sc.nextInt();
-        boolean flag = true;   //git
+        boolean flag = true;   
 
         for(int i=2; i<=n-1; i++){
             if(n%i == 0){
